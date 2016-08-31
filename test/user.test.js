5575f5c7abbfd155e4f5f96d8d97054368af337d
Loay <loay@users.noreply.github.com>
2016-08-31 10:15:00 -0400

    Update test case

--- test/user.test.js
+++ test/user.test.js
@@ -1828,14 +1828,17 @@ describe('User', function() {
           function(next) {
             User.findById(usersId, function(err, userFound)  {
               if (err) return next(err);
-              userFound.updateAttribute('email', 'c@d.com', function(err, userInstance){
+              userFound.updateAttribute('email', 'c@d.com', function(err, userInstance) {
                 if (err) return next(err);
-                AccessToken.find({ where: { userId: usersId }}, function(err, tokens) {
-                  if (err) return next(err);
-                  expect(tokens.length).to.equal(0);
-                  next();
-                });
+                next();
               });
+            });
+          },
+          function(next) {
+            AccessToken.find({ where: { userId: usersId }}, function(err, tokens) {
+              if (err) return next(err);
+              expect(tokens.length).to.equal(0);
+              next();
             });
           },
         ], function(err) {
